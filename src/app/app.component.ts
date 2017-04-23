a178e7c8b8101c22f5f07108c1dd0cab338a9f4c
pimtegelaar <pimtegelaar@gmail.com>
2017-04-23 14:15:25 +0200

    Add init, load commits and get current commit. Add list of commits with option to set current commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CommitService} from "./commit-service";
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Git replay';
+  currentCommit = null;
+  currentIndex = 0;
+
+  commits = [];
+
+  constructor(private commitService: CommitService) {
+
+  }
+
+  onInit() {
+    this.commitService.init().subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    );
+  }
+
+  updateCurrentCommit(currentCommit: any) {
+    this.currentCommit = currentCommit;
+    this.currentIndex = this.commitIndexOf(currentCommit);
+  }
+
+  commitIndexOf(o: any) {
+  for (let i = 0; i < this.commits.length; i++) {
+    if (this.commits[i].name == o.name) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+  onSetCurrentCommit(currentCommit: any) {
+    this.updateCurrentCommit(currentCommit);
+    this.commitService.setCurrentCommit(currentCommit.name).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error));
+  }
+
+  onGetCurrentCommit() {
+    this.commitService.getCurrentCommit().subscribe(
+      (currentCommit: any) => {
+        this.updateCurrentCommit(currentCommit);
+      },
+      (error) => console.log(error)
+    );
+
+  }
+
+  onGetCommits() {
+    this.commitService.getCommits().subscribe(
+      (commits: any[]) => {
+        this.commits = commits;
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  getColor(index: number) {
+    if (this.currentIndex > index) return '#e6fee6';
+    if (this.currentIndex < index) return 'white';
+    return 'lightyellow';
+  }
 }
---
+++ src/app/commit-service.ts
@@ -0,0 +1,50 @@
+import {Injectable} from "@angular/core";
+import {Http, Response} from "@angular/http";
+import 'rxjs/Rx';
+import {Observable} from "rxjs";
+
+@Injectable()
+export class CommitService {
+
+  private baseUrl: string = "http://localhost:8080/gitreplay/build";
+
+  constructor(private http: Http) {
+  }
+
+  init() {
+    return this.http.post(this.baseUrl + "/init", null)
+  }
+
+  setCurrentCommit(commit: string) {
+    return this.http.post(this.baseUrl + "/current-commit/" + commit, null)
+  }
+
+  getCurrentCommit() {
+    return this.http.get(this.baseUrl + "/current-commit")
+      .map(
+        (response: Response) => {
+          return response.json();
+        }
+      ).catch(
+        (error: Response) => {
+          console.log(error);
+          return Observable.throw(error);
+        }
+      );
+  }
+
+  getCommits() {
+    return this.http.get(this.baseUrl + "/commits")
+      .map(
+        (response: Response) => {
+          return response.json();
+        }
+      ).catch(
+        (error: Response) => {
+          console.log(error);
+          return Observable.throw(error);
+        }
+      );
+  }
+
+}
