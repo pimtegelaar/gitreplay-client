908f37c10bc5eb135309c6655b3a45014c28ad4a
pimtegelaar <pimtegelaar@gmail.com>
2017-05-04 23:17:43 +0200

    update gitreplay port

--- src/app/commit-service.ts
+++ src/app/commit-service.ts
@@ -6,7 +6,7 @@ import {Observable} from "rxjs";
 @Injectable()
 export class CommitService {
 
-  private baseUrl: string = "http://localhost:8080/gitreplay/build";
+  private baseUrl: string = "http://localhost:8090/gitreplay/build";
 
   constructor(private http: Http) {
   }
